34118a74c9774c6e0ab148fb72f541edcf29f943
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-15 23:40:35 -0400

    Utilizando lazy para crear listas infinitas, con el protocolo Sequence

--- Lazy-evaluation.playground/Contents.swift
+++ Lazy-evaluation.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var str = "Hello, playground"
+struct ValidPrices: Sequence, IteratorProtocol {
+    let maxPrice: Int
+    let multiplier = 10
+    
+    var current: Int
+    var nextValue: Int
+    
+    init(_ range: ClosedRange<Int>) {
+        current = range.lowerBound
+        nextValue = range.lowerBound * multiplier
+        maxPrice = range.upperBound
+    }
+    
+    mutating func next() -> String? {
+        guard current <= maxPrice else {
+            return nil
+        }
+        
+        let result = current
+        current = nextValue
+        nextValue = nextValue * multiplier
+        return "\(result - 1).95 €"
+    }
+}
+// Utilizando lazy para crear listas infinitas, con el protocolo Sequence
+for price in ValidPrices(1...500) {
+    print(price)
+}
